50ec29b1e543cf849d06ab572818d4498ae26799
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2022-04-16 02:39:56 -0400

    Add files via upload

--- Sprite Detection/RD_detectdata.py
+++ Sprite Detection/RD_detectdata.py
@@ -31,7 +31,6 @@ def RD_Function(filepath, keypath):
 
         keys = dict(zip(i_list, lines2))
 
-    print()
     for file in os.listdir(filepath):
         with open(str(filepath + "/" + file)) as f:
             lines = f.readlines()
@@ -65,15 +64,51 @@ def RD_Function(filepath, keypath):
         row = [video, frame, character, action, desc, conf]
         df.loc[len(df)] = row
 
-    print(df)
     return df
 
 A = RD_Function(P1_filepath, P1_keypath)
 B = RD_Function(P2_filepath, P2_keypath)
+A = A.sort_values(by=['frame'])
+B = B.sort_values(by=['frame'])
 
 C = pd.merge(A, B, on=['video',  'frame'])
 C = C.sort_values(by=['frame'])
-
-print(C)
-
 C.to_csv('RD_proto.csv', index=False)
+
+# ------------------------------------------ ACTION MERGING ---------------------------------------------------------------
+
+def action_merge(df):
+    new_df = pd.DataFrame(columns = ["startup_frame", "ending_frame", "total_frames", "character", "action"])
+    prev_act = df["action"].iloc[0]
+    firstframe = 1
+    lastframe = ""
+    duration = 0
+    count = 0
+
+    for index, row in df.iterrows():
+        if(prev_act != row["action"] or count == len(df) - 1): # If previous action is different from current, stop tracking current move
+            totalframe = lastframe - firstframe
+            if duration == 0: # Only appears for one frame
+                firstframe = lastframe
+                totalframe = 1
+            new_row = {"startup_frame": firstframe, "ending_frame": lastframe, "total_frames": totalframe,  "character": row["character"],  "action": prev_act}
+            new_df.loc[len(new_df)] = new_row
+
+            # print(firstframe, [firstframe, lastframe], prev_act, duration)
+            # print("----------------------------------")
+
+            # Reset for next move
+            prev_act = row["action"]
+            firstframe = row["frame"]
+            duration = 0
+        elif prev_act == row["action"]: # If previous action is the same
+            lastframe = row["frame"]
+            duration = duration + 1
+        count = count + 1
+    return new_df
+
+A2 = action_merge(A)
+B2 = action_merge(B)
+C2 = pd.merge(A2, B2, on = "startup_frame", how = "outer")
+C2 = C2.sort_values(by = ["startup_frame"])
+C2.to_csv('RD_prod2.csv', index=False)
