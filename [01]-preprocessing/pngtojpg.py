230142b8dca0fe129737b858bac8e88ead92823e
Tyler E <69095276+Tyler-Edwards-E@users.noreply.github.com>
2024-08-27 21:26:05 -0400

    Add files via upload

---
+++ [01]-preprocessing/pngtojpg.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import os
+
+
+model_name = 'chun-2024-06-26'
+# Filepath to train images folder
+train_folder = "YARDS-SF3rdStrike/" + model_name + "/images/train"
+# Output for jpg versions of train images
+output_train_folder = "YARDS-SF3rdStrike/" + model_name + "/images/train-jpg"
+
+# Filepath to val images folder
+val_folder = "YARDS-SF3rdStrike/" + model_name + "/images/val"
+# Output for jpg versions of val images
+output_val_folder = "YARDS-SF3rdStrike/" + model_name + "/images/val-jpg"
+
+# Create the output folders if they don't exist
+if not os.path.exists(output_train_folder):
+    os.makedirs(output_train_folder)
+if not os.path.exists(output_val_folder):
+    os.makedirs(output_val_folder)
+
+def pngtojpg(input, output):
+    c = 1 # Loop count
+    # Loop through all pngs in the input folder
+    for filename in os.listdir(input):
+        print(c)
+        print(filename)
+        print()
+        if filename.endswith(".png"):
+            # Open the image
+            image = Image.open(os.path.join(input, filename))
+
+            # Get the filename without the extension
+            basename, _ = os.path.splitext(filename)
+            image = image.convert('RGB')
+            # Save the image as a JPG with 80% quality (adjustable)
+            image.save(os.path.join(output, basename + ".jpg"), quality=85)
+        c = c + 1
+
+pngtojpg(train_folder, output_train_folder)
+pngtojpg(val_folder, output_val_folder)
+
+print("All .png images have been converted to .jpg images.")
---
+++ [01]-preprocessing/yards.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\Ty\3D Objects\~RD\Data Generation\YARDS-SF3rdStrike\~yaml_files"
+yards -c chun-config.yaml
+pause
